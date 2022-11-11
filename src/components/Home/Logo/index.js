e522c42efccc642c3c024a47598b3e3c120a2142
Derek <derekxu888@gmail.com>
2022-11-11 04:15:51 -0500

    Fixed gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -10,38 +10,38 @@ const Logo = () => {
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+    // useEffect(() => {
+    //     gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        }).from(outlineLogoRef.current, {
-            drawSVG: 5,
-            duration: 20,
-        })
+    //     gsap.timeline()
+    //     .to(bgRef.current, {
+    //         duration: 1,
+    //         opacity: 1,
+    //     }).from(outlineLogoRef.current, {
+    //         drawSVG: 5,
+    //         duration: 20,
+    //     })
 
-        gsap.fromTo(
-          outlineLogoRef.current, {
-            opacity: 1,
-          }, {
-            opacity: 1,
-            delay: 5,
-            duration: 3,
-          }
-        )
+    //     gsap.fromTo(
+    //       outlineLogoRef.current, {
+    //         opacity: 1,
+    //       }, {
+    //         opacity: 1,
+    //         delay: 5,
+    //         duration: 3,
+    //       }
+    //     )
 
-        // gsap.fromTo(
-        //     solidLogoRef.current, {
-        //         opacity: 0,
-        //     }, {
-        //         opacity: 1,
-        //         delay: 4,
-        //         duration: 4,
-        //     }
-        // )
-    }, [])
+    //     // gsap.fromTo(
+    //     //     solidLogoRef.current, {
+    //     //         opacity: 0,
+    //     //     }, {
+    //     //         opacity: 1,
+    //     //         delay: 4,
+    //     //         duration: 4,
+    //     //     }
+    //     // )
+    // }, [])
 
     return (
         <div className="logo-container" ref={bgRef}>
