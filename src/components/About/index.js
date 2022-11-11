70c86b1845c8a351d1505b88c757a3f42dcffb6d
Derek <derekxu888@gmail.com>
2022-11-11 00:48:51 -0500

    About page animations and cube

---
+++ src/components/About/index.js
@@ -0,0 +1,64 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faAngular } from '@fortawesome/free-solid-svg-icons'
+import { faAngular,
+     faCss3, 
+     faJsSquare,
+     faHtml5,
+     faReact,
+     faGitAlt } from '@fortawesome/free-brands-svg-icons'
+
+
+const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000)
+    }, []);
+
+    const aboutArray = ['A','b','o','u','t',' ','m','e'];
+    return(
+        <div className = 'container about-page'>
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={aboutArray}
+                        idx={15} />
+                </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
